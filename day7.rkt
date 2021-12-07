ba6d33fad78cd54af3053f4a4506b401bf3733fe
John Berthels <john.berthels@gmail.com>
2021-12-07 08:04:23 +0000

    Day 7 part 2

--- day7.rkt
+++ day7.rkt
@@ -18,3 +18,20 @@
                     (stream->list (in-range top))))))
 
 (printf "Min fuel spent is ~a\n" (p1-min-fuel crabs))
+
+; 1 -> 1, 2 -> 1+2, 3 -> 1+2+3
+(define (p2-crab-fuel-to n crab)
+  (let ([d (abs (- n crab))])
+    (/ (* d (add1 d))
+       2)))
+
+(define (p2-crabs-fuel-to n crabs)
+  (apply + (map (lambda (c) (p2-crab-fuel-to n c))
+                crabs)))
+
+(define (p2-min-fuel crabs)
+  (let ([top (length crabs)])
+    (apply min (map (lambda (n) (p2-crabs-fuel-to n crabs))
+                    (stream->list (in-range top))))))
+
+(printf "P2 Min fuel spent is ~a\n" (p2-min-fuel crabs))
