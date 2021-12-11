6d62f38a9c1d83600496d8ae5824c8308decbaf7
John Berthels <john.berthels@gmail.com>
2021-12-11 14:42:10 +0000

    Day 9 Part 2

--- day9.rkt
+++ day9.rkt
@@ -31,3 +31,33 @@
 (printf "Sum risk levels ~a\n" (apply +
                                       (map (lambda (p) (risk-level g p))
                                            low-points)))
+
+
+; Assume all basins bounded by 9s
+; So don't need to implement basin-merging, each low point
+; point will flood out to one basin
+(define (get-basin g p)
+  (define (helper last-pts seen)
+;    (printf "\nlast-pts ~a seen ~a\n" last-pts seen)
+    (let* ([new-seen (set-union seen (list->set last-pts))]
+           [ns (flatten (map (lambda (p) (grid-neighbour-points g p))
+                             last-pts))]
+           [new-pts (filter (lambda (x)
+                              (and (not (set-member? new-seen x))
+                                   (< (grid-get g x) 9)))
+                            ns)])
+;      (printf "ns ~a new-pts ~a\n" ns new-pts)
+      (if (empty? new-pts)
+          new-seen
+          (helper new-pts new-seen))))
+  (helper (list p) (set)))
+
+(define (basin-size g p)
+  (length (set->list (get-basin g p))))
+
+(define basin-sizes (map (lambda (p) (basin-size g p))
+                         low-points))
+
+(printf "Basin sizes ~a\n" basin-sizes)
+
+(printf "Mult 3 biggest ~a\n" (apply * (take (sort basin-sizes >) 3)))
