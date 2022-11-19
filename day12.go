fde6f118fe21339e6fd9911670a680e193b0c7c3
John Berthels <john.berthels@gmail.com>
2022-11-19 14:13:52 +0000

    Day12 part 1 in go

--- day12.go
+++ day12.go
@@ -10,6 +10,13 @@ type Day12 struct{}
 
 type Vertex string
 
+func (v Vertex) IsSmall() bool {
+	if v == "" {
+		panic("Empty string vertex")
+	}
+	return string(v) == strings.ToLower(string(v))
+}
+
 type Edge struct {
 	a, b Vertex
 }
@@ -34,6 +41,42 @@ func (g Graph) String() string {
 	return b.String()
 }
 
+func (g Graph) CountPaths(from, to Vertex) int {
+	if !g.IsVertex(from) || !g.IsVertex(to) {
+		panic(fmt.Sprintf("Internal error: [%s] or [%s] not a vertex", from, to))
+	}
+	visited := make(map[Vertex]int)
+	return g.countPaths(from, to, visited)
+}
+
+func (g Graph) countPaths(from, to Vertex, visited map[Vertex]int) int {
+	if from == to {
+		return 1
+	}
+	visited[from]++
+	neighbours := g.Neighbours(from)
+
+	canVisit := func(v Vertex) bool {
+		if v.IsSmall() {
+			return visited[v] == 0
+		}
+		return true
+	}
+
+	neighbours = Filter(canVisit, neighbours)
+
+	count := 0
+	for _, neighbour := range neighbours {
+		visitedCopy := make(map[Vertex]int)
+		for k, v := range visited {
+			visitedCopy[k] = v
+		}
+		count += g.countPaths(neighbour, to, visitedCopy)
+	}
+	//	fmt.Printf("[%s -> %s]: neighbours [%s] returning %d\n", from, to, neighbours, count)
+	return count
+}
+
 func NewGraphFromEdges(edges []Edge) *Graph {
 	g := Graph{}
 	for _, edge := range edges {
@@ -41,6 +84,15 @@ func NewGraphFromEdges(edges []Edge) *Graph {
 		g.addEdge(edge.Reverse())
 	}
 	return &g
+}
+
+func (g Graph) Neighbours(v Vertex) []Vertex {
+	return g[v].ToList()
+}
+
+func (g Graph) IsVertex(v Vertex) bool {
+	_, ok := g[v]
+	return ok
 }
 
 func (g Graph) addEdge(e Edge) {
@@ -71,5 +123,7 @@ func (d *Day12) Run(out io.Writer, lines []string) error {
 	fmt.Fprintf(out, "Edges: %v\n", edges)
 	g := NewGraphFromEdges(edges)
 	fmt.Fprintf(out, "G:\n%v\n", g)
+	numPaths := g.CountPaths("start", "end")
+	fmt.Fprintf(out, "Num paths: %d\n", numPaths)
 	return nil
 }
