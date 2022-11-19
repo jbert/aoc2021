9f903cefc6b5be2e729184e55c33a83fcf781e9f
John Berthels <john.berthels@gmail.com>
2022-11-19 14:37:45 +0000

    grid from lines

--- day15.go
+++ day15.go
@@ -3,6 +3,8 @@ package aoc
 import (
 	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
 
 type Day15 struct{}
@@ -14,5 +16,28 @@ func NewDay15() *Day15 {
 
 func (d *Day15) Run(out io.Writer, lines []string) error {
 	fmt.Fprintf(out, "Running\n")
+	grid := GridFromLines(lines)
+	fmt.Printf("Grid:\n%v\n", grid)
 	return nil
+}
+
+type Grid [][]int
+
+func GridFromLines(lines []string) Grid {
+
+	var grid [][]int
+	for _, line := range lines {
+		var row []int
+		digits := strings.Split(line, "")
+		for _, digit := range digits {
+			num, err := strconv.Atoi(digit)
+			if err != nil {
+				panic(fmt.Sprintf("Bad digit? [%s]: %s", digit, err))
+			}
+			row = append(row, num)
+		}
+		grid = append(grid, row)
+	}
+
+	return grid
 }
