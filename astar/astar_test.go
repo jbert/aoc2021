638603e1b3745b537fc86d623a340ef249ea9351
John Berthels <john.berthels@gmail.com>
2022-11-20 12:05:49 +0000

    Add failing test for astar

---
+++ astar/astar_test.go
@@ -0,0 +1,25 @@
+package astar
+
+import (
+	"testing"
+
+	"github.com/jbert/aoc2021/graph"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAstar(t *testing.T) {
+	a := assert.New(t)
+	edges := []graph.Edge[int]{
+		{1, 2, 2.0},
+		{1, 3, 1.0},
+		{2, 4, 1.0},
+		{3, 4, 1.0},
+	}
+	g := graph.NewFromEdges(edges, false)
+
+	one := func(v int) float64 { return 1.0 }
+	gg := (Graph[int])(g)
+	path, err := Astar(1, 4, gg, one)
+	a.NoError(err, "No error in search")
+	a.Equal([]int{1, 3, 4}, path)
+}
